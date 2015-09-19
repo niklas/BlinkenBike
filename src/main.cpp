d9c5e757be90af31ff93ee381f36cb5ea4b0e22d
Niklas Hofer <niklas+dev@lanpartei.de>
2015-09-19 22:39:59 +0200

    copy adafruit longstrandtest

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,56 @@
   #include "Arduino.h"
 #endif
 
-#ifndef LED_PIN
-  // Most Arduino boards already have a LED attached to pin 13 on the board itself
-  #define LED_PIN 13
-#endif
+#include "LPD8806.h"
+#include "SPI.h"
+
+#define ON_BOARD_LED 13
+
+
+// Number of RGB LEDs in strand
+int nLEDs = 32;
+
+// Chose 2 pins for output; can be any valid output pins:
+int dataPin  = 2;
+int clockPin = 3;
+
+// First parameter is the number of LEDs in the strand.  The LED strips
+// are 32 LEDs per meter but you can extend or cut the strip.  Next two
+// parameters are SPI data and clock pins:
+LPD8806 strip = LPD8806(nLEDs, dataPin, clockPin);
 
 void setup()
 {
-  pinMode(LED_PIN, OUTPUT);     // set pin as output
+  pinMode(ON_BOARD_LED, OUTPUT);     // set pin as output
+  strip.begin();
+  strip.show();
 }
 
-void loop()
-{
-  digitalWrite(LED_PIN, HIGH);  // set the LED on
-  delay(1000);                  // wait for a second
-  digitalWrite(LED_PIN, LOW);   // set the LED off
-  delay(500);                  // wait for a second
+// Chase one dot down the full strip.  Good for testing purposes.
+void colorChase(uint32_t c, uint8_t wait) {
+  int i;
+
+  digitalWrite(ON_BOARD_LED, HIGH);
+
+  // Start by turning all pixels off:
+  for(i=0; i<strip.numPixels(); i++) strip.setPixelColor(i, 0);
+
+  // Then display one pixel at a time:
+  for(i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c); // Set new pixel 'on'
+    strip.show();              // Refresh LED states
+    strip.setPixelColor(i, 0); // Erase pixel, but don't refresh!
+    delay(wait);
+  }
+
+  strip.show(); // Refresh to turn off last pixel
+  digitalWrite(ON_BOARD_LED, LOW);   // set the LED off
+  delay(wait);
+}
+
+void loop() {
+  colorChase(strip.Color(127,  0,  0), 100); // Red
+  colorChase(strip.Color(  0,127,  0), 100); // Green
+  colorChase(strip.Color(  0,  0,127), 100); // Blue
+  colorChase(strip.Color(127,127,127), 100); // White
 }
