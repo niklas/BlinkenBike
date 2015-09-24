fdbbcc8490131e1412983bab6adce844eed5c571
Niklas Hofer <niklas+dev@lanpartei.de>
2015-09-24 22:30:02 +0200

    soldered buttons devide to other voltages

--- src/buttons.cpp
+++ src/buttons.cpp
@@ -15,17 +15,20 @@ void dispatchButtons() {
   val = analogRead(PIN_BUTTONS);
 
 
-  if (val > 1010) {
+  if (val > 960) {
     button = BUTTON_1;
-  } else if (val > 930) {
+  } else if (val > 910) {
     button = BUTTON_2;
   } else if (val > 510) {
     button = BUTTON_3;
   }
 
-  Serial.print(val);
-  Serial.print("  ");
-  Serial.println(button);
+  if (button != BUTTON_NONE) {
+    Serial.print(button);
+    Serial.print("  ");
+    Serial.print(val);
+    Serial.println();
+  }
 }
 
 
